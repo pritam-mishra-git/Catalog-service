4e7d9cd69357b1f60d46d8304ec36427799db68f
pritam-mishra-git <pritam.mishra@rumango.com>
2025-02-21 11:52:12 +0530

    initial commit

---
+++ src/test/java/com/springboot/springbootmicroservice/SpringbootMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.springbootmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
